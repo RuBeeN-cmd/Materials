7278e27df7f091667d2a40d96cf42fcdabaff14e
Ruben Rollin <rrollin@2C7.42angouleme.fr>
2022-12-12 12:42:28 +0100

    Mlx initialization finished

--- include/materials.h
+++ include/materials.h
@@ -2,5 +2,22 @@
 #define MATERIALS_H
 
 #include "mlx.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+typedef struct	s_data
+{
+	void	*mlx;
+	void	*win;
+	int		width;
+	int		height;
+}				t_data;
+
+// data/init.c
+t_data	init_data(char title[], int width, int height);
+void	destroy_data(t_data data);
+
+// data/hook.c
+int		key_hook(int keycode, t_data *data);
 
 #endif
--- src/main.c
+++ src/main.c
@@ -2,5 +2,8 @@
 
 int main(void)
 {
+	t_data	data = init_data("Materials", 500, 250);
+	mlx_loop(data.mlx);
+	destroy_data(data);
 	return (0);
 }
